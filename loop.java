2739962f3b8d25524565c54d0d31fc1d488fe2ae
netkaycoded <73316813+netkaycoded@users.noreply.github.com>
2021-07-14 11:26:52 -0500

    Update loop.java

--- loop.java
+++ loop.java
@@ -3,7 +3,7 @@ public class MyClass {
       System.out.println("MATRIC NUM:- CS20190201XXXX");
       System.out.println("NAME :- OLADEXXXXXXXX");
       int i, Num = 10, Sum = 0;
-      for(i = 1; i<=Num; ++i ){
+      for(i = 1; i <= Num; ++i ){
           Sum = Sum + i;
       }
         System.out.println("Sum ="+ Sum);
